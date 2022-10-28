092b39582a1e4db3b045ebd5c9d59dd93499cdc5
yuan <youremail@yourdomain.com>
2022-10-28 23:44:41 +0800

    Wds do not work with multinode.

--- train_h.sh
+++ train_h.sh
@@ -1,7 +1,7 @@
 python main.py \
     -t \
-    --base configs/stable-diffusion/fp16-no-ema-finetune-wds.yaml \
-    --gpus 0, \
+    --base configs/stable-diffusion/fp16-no-ema-finetune.yaml \
+    --gpus 0,1 \
     --scale_lr False \
     --num_nodes 1 \
     --finetune_from /root/autodl-tmp/v1-5-pruned-no_ema.ckpt \
